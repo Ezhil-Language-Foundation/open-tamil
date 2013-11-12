141ff675381863c8dfda97305662acc4f2d61743
Muthiah Annamalai <arcturusannamalai@gmail.com>
2013-11-11 22:04:54 -0500

    use UTF8 encoded unicode strings everywhere; update tests to show discrete letters used"

--- test/letter_tests.py
+++ test/letter_tests.py
@@ -12,15 +12,15 @@ import tamil.utf8 as utf8
 class Letters(unittest.TestCase):
     def test_uyirmei(self):     
         print(utf8.uyirmei(2))
-        assert( utf8.uyirmei(2)  == "கி" )
+        assert( utf8.uyirmei(2)  == u"கி" )
 
     def test_letter_extract_from_code_pts(self):
-        letters = utf8.get_letters(("கூவிளம் என்பது என்ன சீர்"))
+        letters = utf8.get_letters(u"கூவிளம் என்பது என்ன சீர்")
         #print "len ==== > " , len(letters)
         assert( len(letters) == 15 )
-        for letter in  letters:
-            print(letter)
-        assert( letter == ("ர்").decode("utf-8") )
+        for pos,letter in  enumerate(letters):
+            print(u"%d %s"%(pos,letter))
+        assert( letter == (u"ர்") )
     
 if __name__ == '__main__':    
     test_support.run_unittest(Letters)
