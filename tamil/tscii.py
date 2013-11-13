252f2df3bb716cddec6d9dcb7a80757672b838d3
Muthiah Annamalai <arcturusannamalai@gmail.com>
2013-11-12 22:16:53 -0500

    # TSCII - Tamil ASCII, like CJK, wide-chars in Windows for Cyrillic, and other # encodings basically map the extended ASCII (128-255) range of the 8-byte # storage into various code points. # # Among various Tamil encodings TSCII was an advancement over font-table # encoding, in the pre-Unicode days.

---
+++ tamil/tscii.py
@@ -0,0 +1,12 @@
+# -*- coding: utf-8 -*-
+# 
+# (C) 2013 Muthiah Annamalai
+# Licensed under GPL Version 3
+#
+ 
+
+# 
+# TSCII library provides the TSCII v1.7 symbol table and mapping to Unicode
+# A converter, for example, could be written based on this information.
+# 
+# Ref: M. Nedumaran, "Text conversion from TSCII 1.7 to Unicode," (2007).
