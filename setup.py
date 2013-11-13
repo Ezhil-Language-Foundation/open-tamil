87cd05a2ec071b9f08908965e42019e0cc7c7f01
Muthiah Annamalai <arcturusannamalai@gmail.com>
2013-11-12 22:06:36 -0500

    version 0.2; added packages for ngram, transliterate - corpora lookup, probability models etc

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ from distutils.core import setup
 from codecs import open
 
 setup(name='Open-Tamil',
-      version='0.1-dev',
+      version='0.2-dev',
       description='Tamil language text processing tools',
       author='Muthiah Annamalai',
       author_email='ezhillang@gmail.com',
