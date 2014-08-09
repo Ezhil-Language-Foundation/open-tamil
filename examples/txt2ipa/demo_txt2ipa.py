14e15fb4e93e4f8631b3bdacd44580d15c670e12
arulalant <arulalant@gmail.com>
2014-08-09 23:28:15 +0530

    updated (c)

--- examples/txt2ipa/demo_txt2ipa.py
+++ examples/txt2ipa/demo_txt2ipa.py
@@ -1,6 +1,11 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
+# (C) 2014 Arulalan.T <arulalant@gmail.com>
+# 
+# This file is part of 'open-tamil/txt2ipa' package examples
+# 
+
 import sys
 sys.path.append('../..')
 
--- examples/txt2unicode/demo_tscii2utf8.py
+++ examples/txt2unicode/demo_tscii2utf8.py
@@ -1,6 +1,11 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
+# (C) 2014 Arulalan.T <arulalant@gmail.com>
+# 
+# This file is part of 'open-tamil/txt2unicode' package examples
+# 
+
 import sys
 sys.path.append('../..')
 from tamil.txt2unicode import tscii2unicode
