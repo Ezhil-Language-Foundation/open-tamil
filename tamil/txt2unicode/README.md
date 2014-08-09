a106b9e30f8727f2d20ca9f96a01320f820d23e8
arulalant <arulalant@gmail.com>
2014-08-09 23:05:18 +0530

    added txt2unicode converters & unicode2encode converters module

---
+++ tamil/txt2unicode/README.md
@@ -0,0 +1,93 @@
+txt2unicode
+===========
+Tamil Text Encode to Unicode Converter.
+
+Don't you know what is your text encode ? Don't worry. This `txt2unicode` will find it & convert to unicode for you :-)
+
+
+Available Tamil Encode Converters
+=================================
+
+| S.No  | எழுத்துரு | Encode Name | To Unicode Converter | To Encode Convereter |
+| ---- | :---------: | :---------: | :---------: | :---------: |
+| 1 | அஞ்சல் | Anjal | anjal2unicode | unicode2anjal|
+| 2 |  பாமினி | Bamini|  bamini2unicode| unicode2bamini|
+| 3 | பூமி  | Boomi  |  boomi2unicode| unicode2boomi| 
+| 4 | தினகரன் | Dinakaran |  dinakaran2unicode | unicode2dinakaran|
+| 5 | தினமணி  | Dinamani  | dinamani2unicode  | unicode2dinamani ||
+| 6 | தினத்தந்தி |Dinathanthy |  dinathanthy2unicode|unicode2dinathanthy|
+| 7 | இன்டோவெப்   | Indoweb    | indoweb2unicode   | unicode2indoweb  |
+| 8 |  கவிபிரியா |  Kavipriya  | kavipriya2unicode| unicode2kavipriya|     
+| 9 | கோயல்என்   |  Koeln      | koeln2unicode    |  unicode2koeln  |
+| 10| லிபி     |  Libi       |  libi2unicode    | unicode2libi   |
+| 11| முரசொலி | Murasoli |  murasoli2unicode | unicode2murasoli |
+| 12| மலை  |  Mylai    |mylai2unicode      | unicode2mylai|
+| 13| நக்கீரன்  |Nakkeeran|     nakkeeran2unicode| unicode2nakkeeran|
+| 14| பழைய விகடன்  | Old Vikatan  | oldvikatan2unicode | unicode2oldvikatan |
+| 15| பல்லவர்  | Pallavar      | pallavar2unicode  | unicode2pallavar | 
+| 16| ரோமன்   | Roman   | roman2unicode  | unicode2roman |
+| 17| டேப்    | Tab  | tab2unicode  | unicode2tab|
+| 18| டாம்   |  Tam  |tam2unicode | unicode2tam|
+| 19| டிஸ்கி |Tscii  |    tscii2unicode|   unicode2tscii|       
+| 20| வெப்உலகம்   | Webulagam | webulagam2unicode |  unicode2webulagam |
+| 21| **கண்டுபுடி**| **AutoFind**    | **auto2unicode**|       **unicode2auto**           |
+
+
+
+
+Auto Find Input Encode & Convert to Unicode
+===========================================
+
+  `auto2unicode` function will try to find encode of input text. If it is found, then it will convert input text to unicode using appropriate encode converters among available encode converters.
+  
+  **Out of 20 encodes, 15 encodes can be found by this `auto2unicode`.** 
+  
+  Except `dinamani`, `nakkeeran` , `murasoli` , `tam` & `webulagam` encodes, `auto2unicode` function can find input text's encode and will convert it into unicode. [Why?](../../examples/txt2unicode/encodes_chars/README.md)
+  
+  Look at demo of [tscii2unicode](../../examples/txt2unicode/demo_tscii2utf8.py) converter
+
+  Look at demo of [auto2unicode](../../examples/txt2unicode/demo_auto2utf8.py) converter
+  
+  Look at **limitation of `auto2unicode`** [here](../../examples/txt2unicode/encodes_chars/README.md)
+  
+  
+
+  
+Convert From Unicode to Encode
+==============================
+  Here reverse engine of `encode2unicode` used to convert back to encode from unicode.
+  
+  Look at above table for available `unicode2encode` functions.
+  
+  Look at demo of [unicode2tscii](../../examples/txt2unicode/demo_utf8_2_tscii.py) converter
+  
+  Look at demo of [unicode2auto](../../examples/txt2unicode/demo_utf8_2_auto.py) converter
+  
+
+Test Status:
+===========
+  1. auto2unicode works
+  2. tscii2unicode works
+  3. unicode2tscii works
+   
+
+
+Todo:
+====
+  * Need to test all the above encodes
+  * Need to add fonts2unicode converter for the above encodings
+  
+
+RoadMap:
+=======
+  * Look roadmap of txt2unicode [here](https://github.com/arulalant/txt2unicode/wiki/RoadMap)
+  
+
+Credits:
+=======
+  Thanks to http://kandupidi.com/converter/ and http://www.suratha.com/reader.htm online tools.
+  
+Regards,
+
+Arulalan.T
+  
---
+++ tamil/txt2unicode/unicode2encode.py
@@ -0,0 +1,172 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+
+##############################################################################
+# (C) 2014 Arulalan.T <arulalant@gmail.com>                                  #
+#                                                                            #
+# Author : Arulalan.T <arulalant@gmail.com>                                  #
+# Date : 08.08.2014                                                          #
+#                                                                            #
+# This file is part of open-tamil/txt2unicode                                #
+#                                                                            #
+# txt2unicode is free software: you can redistribute it and/or               #
+# modify it under the terms of the GNU General Public License as published by#
+# the Free Software Foundation, either version 3 of the License, or (at your #
+# option) any later version. This program is distributed in the hope that it #
+# will be useful, but WITHOUT ANY WARRANTY; without even the implied warranty#
+# of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU General#
+# Public License for more details. You should have received a copy of the GNU#
+# General Public License along with this program. If not, see                #
+# <http://www.gnu.org/licenses/>.                                            #
+#                                                                            #
+##############################################################################
+
+from encode2utf8 import anjal2utf8, bamini2utf8, boomi2utf8, \
+    dinakaran2utf8, dinamani2utf8, dinathanthy2utf8, \
+    kavipriya2utf8, murasoli2utf8, mylai2utf8, nakkeeran2utf8, \
+    roman2utf8, tab2utf8, tam2utf8, tscii2utf8, pallavar2utf8, \
+    indoweb2utf8, koeln2utf8, libi2utf8, oldvikatan2utf8, webulagam2utf8
+
+from encode2unicode import _all_encodes_, _get_unique_ch, \
+                                _get_unique_common_encodes
+
+__all__ = ['unicode2anjal', 'unicode2bamini', 'unicode2boomi', 
+    'unicode2dinakaran', 'unicode2dinathanthy', 'unicode2kavipriya',
+    'unicode2murasoli', 'unicode2mylai', 'unicode2nakkeeran',
+    'unicode2roman', 'unicode2tab', 'unicode2tam', 'unicode2tscii',
+    'unicode2indoweb', 'unicode2koeln', 'unicode2libi', 'unicode2oldvikatan',
+    'unicode2webulagam', 'unicode2auto', 'unicode2dinamani', 'unicode2pallavar']
+
+
+def unicode2encode(text, charmap):
+    '''
+    charmap : dictionary which has both encode as key, unicode as value
+    '''
+    if isinstance(text, (list, tuple)):
+        unitxt = ''
+        for line in text:
+            for val,key in charmap.iteritems():
+                if key in line:
+                    line = line.replace(key, val)
+                # end of if val in text:
+            unitxt += line
+        # end of for line in text:
+        return unitxt
+    elif isinstance(text, str):
+        for val,key in charmap.iteritems():
+            if key in text:
+                text = text.replace(key, val)
+        # end of for val,key in charmap.iteritems():
+        return text
+    # end of if isinstance(text, (list, tuple)):
+# end of def unicode2encode(text, charmap):
+
+def unicode2anjal(text):
+    return unicode2encode(text, anjal2utf8)
+
+def unicode2bamini(text):
+    return unicode2encode(text, bamini2utf8)
+
+def unicode2boomi(text):
+    return unicode2encode(text, boomi2utf8)
+
+def unicode2dinakaran(text):
+    return unicode2encode(text, dinakaran2utf8)
+
+def unicode2dinamani(text):
+    return unicode2encode(text, dinamani2utf8)
+
+def unicode2dinathanthy(text):
+    return unicode2encode(text, dinathanthy2utf8)
+
+def unicode2kavipriya(text):
+    return unicode2encode(text, kavipriya2utf8)
+
+def unicode2murasoli(text):
+    return unicode2encode(text, murasoli2utf8)
+
+def unicode2mylai(text):
+    return unicode2encode(text, mylai2utf8)
+
+def unicode2nakkeeran(text):
+    return unicode2encode(text, nakkeeran2utf8)
+
+def unicode2roman(text):
+    return unicode2encode(text, roman2utf8)
+
+def unicode2tab(text):
+    return unicode2encode(text, tab2utf8)
+
+def unicode2tam(text):
+    return unicode2encode(text, tam2utf8)
+
+def unicode2tscii(text):
+    return unicode2encode(text, tscii2utf8)
+
+def unicode2pallavar(text):
+    return encode2unicode(text, pallavar2utf8)
+
+def unicode2indoweb(text):
+    return encode2unicode(text, indoweb2utf8)
+
+def unicode2koeln(text):
+    return encode2unicode(text, koeln2utf8)
+
+def unicode2libi(text):
+    return encode2unicode(text, libi2utf8)
+
+def unicode2oldvikatan(text):
+    return encode2unicode(text, oldvikatan2utf8)
+
+def unicode2webulagam(text):
+    return encode2unicode(text, webulagam2utf8)
+
+def unicode2auto(unicode_text, encode_text):
+    """
+    This function will convert unicode (first argument) text into other
+    encodes by auto find the encode (from available encodes) by using sample
+    encode text in second argument of this function.
+
+    unicode_text : Pass unicode string which has to convert into other encode.
+    encode_text : Pass sample encode string to identify suitable encode for it.
+
+    This function tries to identify encode in available encodings.
+    If it finds, then it will convert unicode_text into encode string.
+
+    Author : Arulalan.T
+
+    08.08.2014
+
+    """
+
+    _all_unique_encodes_, _all_common_encodes_ = _get_unique_common_encodes()
+    # get unique word which falls under any one of available encodes from
+    # user passed text lines
+    unique_chars = _get_unique_ch(encode_text, _all_common_encodes_)
+    # count common encode chars
+    clen = len(_all_common_encodes_)
+    msg = "Sorry, couldn't find encode :-(\n"
+    msg += 'Need more words to find unique encode out side of %d ' % clen
+    msg += 'common compound characters'
+    if not unique_chars:
+        print msg
+        return ''
+    # end of if not unique_chars:
+
+    for encode_name, encode_keys in _all_unique_encodes_:
+        if not len(encode_keys): continue
+        for ch in encode_keys:
+            # check either encode char is presnent in word
+            if ch in unique_chars:
+                # found encode
+                print "Whola! found encode : ", encode_name
+                encode = _all_encodes_[encode_name]
+                return unicode2encode(unicode_text, encode)
+            # end of if ch in unique_chars:
+        # end of ifor ch in encode_keys:
+    else:
+        print msg
+        return ''
+    # end of for encode in _all_unique_encodes_:
+# end of def auto2unicode(text):
+
