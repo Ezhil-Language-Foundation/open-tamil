f11126ce9801e0989690883017a8f1925e18a9ad
arulalant <arulalant@gmail.com>
2014-12-15 17:33:43 +0530

    enable sphnix kind of doc style

--- conf.py
+++ conf.py
@@ -37,7 +37,7 @@ if on_rtd:
 else:
     html_theme = 'nature'
     
-html_style = ''
+html_style = '/default.css'
 
 # Add any paths that contain templates here, relative to this directory.
 templates_path = ['_templates']
