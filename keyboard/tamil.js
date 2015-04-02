cf8fc37f5a5fdca8766b82f0ca585c67f59ccb3b
Muthiah Annamalai <ezhillang@gmail.com>
2015-04-01 23:48:06 -0400

    tamil.js : format keys to be modern

--- keyboard/tamil.js
+++ keyboard/tamil.js
@@ -5,7 +5,7 @@
  *  Point to this js file into your page header: <script src="layouts/tamil.js" type="text/javascript"></script>
  *  Initialize the keyboard using: $('input').keyboard({ layout: 'tamil-qwerty' });
  *
- * Copyright 2013, Muthiah Annamalai
+ * Copyright 2013-2015 Muthiah Annamalai
  *
  * Author :  Muthiah Annamalai <ezhillang@gmail.com>
  *
@@ -26,8 +26,9 @@ $.keyboard.layouts['tamil-tamil99-mod'] = {
         /*  அ    இ      உ     ்       எ      க      ப    ம      த      ந      ய  */
 		"\u0b85 \u0b87  \u0b89 \u0bcd  \u0b8e  \u0b95 \u0baa \u0bae \u0ba4 \u0ba8 \u0baf {enter}",
         /*         ஔ    ஒ      ஓ    வ     ங     ல      ர    , . ழ    */
-		"{shift} \u0b94 \u0b93 \u0b92 \u0bb5 \u0b99 \u0bb2 \u0bb0 , . \u0bb4 {shift}",
-		"{accept} {alt} {space} {alt} {cancel}"
+        "{shift} \u0b94 \u0b93 \u0b92 \u0bb5 \u0b99 \u0bb2 \u0bb0 , . \u0bb4 {shift}",
+		/* accept | alt | space | alt | cancel */
+        "{cancel} {alt} {space} {alt} {cancel}"
 	],
 	'shift' : [
          /* numeric key row */
@@ -38,22 +39,23 @@ $.keyboard.layouts['tamil-tamil99-mod'] = {
         /* ௹     ௺    ௸     ஃ  \u0020 \u0020 \u0020 \" : ; \' {enter} */
 		"\u0bf9 \u0bfa \u0bf8 \u0b83 \u0020 \u0020 \u0020 \" : ; \' {enter}",
         /* ௳ ௴ ௵ ௶ ௷ */
-        "{shift} \u0bf3 \u0bf4 \u0bf5 \u0bf6 \u0bf7 \u0020 / \u0020 \u0020 / {shift}",
-		"{accept} {alt} {space} {alt} {cancel}"
-	],
+        "{shift}  \u0bf3 \u0bf4 \u0bf5 \u0bf6 \u0bf7 \u0020 / \u0020 \u0020 / {shift}",
+		/* accept | alt | space | alt | cancel */
+        "{cancel} {alt} {space} {alt} {cancel}"
+    ],
 	'alt' : [
 		"\u0060 1 2 3 4 5 6 7 8 9 0 - \u003D {bksp}",
 		"{tab} q w e r t y u i o p \u005B \u005D \u005C",
 		"a s d f g h j k l ; \u0027 {enter}",
 		"{shift} z x c v b n m , . / {shift}",
-		"{accept} {alt} {space} {alt} {cancel}"
+        "{cancel} {alt} {space} {alt} {cancel}"
 	],
 	'alt-shift' : [
 		"\u007E | @ # $ % \u00ac & * ( ) _ + {bksp}",
 		"{tab} Q W E R T Y U I O P ! \u00a6 \u00a2",
 		"A S D F G H J K L : \"  {enter}",
 		"{shift} Z X C V B N M { } ? {shift}",
-		"{accept} {alt} {space} {alt} {cancel}"
+        "{cancel} {alt} {space} {alt} {cancel}"
 	]
 };
 
@@ -62,24 +64,25 @@ $.keyboard.layouts['tamil-tamil99-mod'] = {
 // ***********************
 if (typeof(language) === 'undefined') { var language = {}; };
 language.tamil = {
+    //translation
 	display : {
 		'a'      : '\u2714:Validate (Shift-Enter)', // check mark - same action as accept
-		'accept' : 'accept (Shift-Enter)',
-		'alt'    : 'Alt:More Characters',
-		'b'      : '\u2190:Backspace',    // Left arrow (same as &larr;)
-		'bksp'   : 'Bksp:Backspace',
+		'accept' : 'ஏற்றுக்கொள் (Shift-Enter)',
+		'alt'    : 'மாற்று:More Characters',
+		'b'      : '\u2190:அழி',    // Left arrow (same as &larr;)
+		'bksp'   : 'அழி:அழி',
 		'c'      : '\u2716:Escape (Esc)', // big X, close - same action as cancel
-		'cancel' : 'Cancel (Esc)',
+		'cancel' : 'நீக்கு',
 		'clear'  : 'C:Clear',             // clear num pad
 		'combo'  : '\u00f6:Toggle Combo Keys',
 		'dec'    : '.:Decimal point',           // decimal point for num pad (optional), change '.' to ',' for European format
 		'e'      : '\u21b5:Enter',        // down, then left arrow - enter symbol
-		'enter'  : 'Enter:Enter',
+		'enter'  : 'ஏற்றுக்கொள்:ஏற்றுக்கொள்',
 		'lock'   : '\u21ea Lock:Caps Lock', // caps lock
 		's'      : '\u21e7:Shift',        // thick hollow up arrow
-		'shift'  : 'Shift:Shift',
+		'shift'  : 'மேல்:மேல்',
 		'sign'   : '\u00b1:Sign for num pad',  // +/- sign for num pad
-		'space'  : 'Space',
+		'space'  : 'இடைவெளி',
 		't'      : '\u21e5:Tab',          // right arrow to bar (used since this virtual keyboard works with one directional tabs)
 		'tab'    : '\u21e5 Tab:Tab'       // \u21b9 is the true tab symbol (left & right arrows)
 	},
