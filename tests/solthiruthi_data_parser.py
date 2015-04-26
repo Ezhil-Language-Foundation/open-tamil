704c1042039c9fec78c81e32ec29022be521bdab
Muthiah Annamalai <ezhillang@gmail.com>
2015-04-26 01:31:38 -0400

    1) solthiruthi CLI 2) tests for solthiruthi CLI using argparse Ref:  Solthiruthi - framework - command line interface #34

--- tests/solthiruthi_data_parser.py
+++ tests/solthiruthi_data_parser.py
@@ -4,6 +4,36 @@
 # setup the paths
 from opentamiltests import *
 from solthiruthi.data_parser import *
+from solthiruthi.solthiruthi import Solthiruthi
+import sys
+
+class CmdLineIO(unittest.TestCase):
+    def test_CLI_interface_help(self):
+        return
+        # find a way to run this test which exits the suite
+        _,parser = Solthiruthi.get_CLI_options(do_parse=False)
+        args = parser.parse_args(['--help'])
+        self.assertEqual(args.help,True)
+    def test_CLI_defaults(self):
+        _,parser = Solthiruthi.get_CLI_options(do_parse=False)
+        args = parser.parse_args( ['-stdin'])
+        self.assertEqual(args.files,'')
+        self.assertEqual(args.help,False)
+        self.assertEqual(args.nalt,10)
+        self.assertEqual(args.Dictionary,['std'])
+        self.assertEqual(args.dialects,['std'])
+        
+    def test_CLI_files(self):
+        ips = ['-files','poonga','vanam','sethu','ezhuthu']
+        _,parser = Solthiruthi.get_CLI_options(do_parse=False)
+        args = parser.parse_args( ips )
+        self.assertEqual(args.files,ips[1:])
+
+    def test_CLI_dicts(self):
+        ips = ['-Dict','std','wikipedia','madurai']
+        _,parser = Solthiruthi.get_CLI_options(do_parse=False)
+        args = parser.parse_args( ips )
+        self.assertEqual(args.Dictionary,ips[1:])
 
 class DataParserTest(unittest.TestCase):
     def test_worlists(self):
