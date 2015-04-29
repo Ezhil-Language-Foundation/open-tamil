ba69384b2d2bebf967f71557ecd4f89ff8820c8b
Muthiah Annamalai <ezhillang@gmail.com>
2015-04-28 20:38:59 -0400

    1) use resource class for refering to data 2) solthiruthi.resources has dictionary pointing from name -> data.

---
+++ solthiruthi/resources.py
@@ -0,0 +1,33 @@
+## -*- coding: utf-8 -*-
+## (C) 2015 Muthiah Annamalai
+## 
+from __future__ import print_function
+
+import os
+
+def get_data_dir():
+    dirname, filename = os.path.split(os.path.abspath(__file__))
+    return os.path.sep.join([dirname,u'data'])
+
+def get_data_dictionaries( ):
+    srcfiles = [u'tamilvu_dictionary_words.txt']
+    return {srcfile.split(u".txt")[0] : mk_path( srcfile ) for srcfile in srcfiles }
+
+def get_data_categories( ):
+    srcfiles = [u'peyargal.txt',
+                u'capitals-n-countries.txt',
+                u'maligaiporul.txt',
+                u'mooligaigal.txt',
+                u'nagarangal.txt',
+                u'palam.txt',
+                u'vilangugal.txt']
+    return { srcfile.split(u".txt")[0] : mk_path( srcfile ) for srcfile in srcfiles }
+
+DATADIR = get_data_dir()
+
+def mk_path( srcfile ):
+    return os.path.sep.join( [DATADIR, srcfile] )
+
+CATEGORY_DATA_FILES = get_data_categories( )
+
+DICTIONARY_DATA_FILES = get_data_dictionaries( )
