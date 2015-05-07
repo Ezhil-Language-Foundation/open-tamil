bb2d440a6f9a065f34ffc97943163d010d83c282
Muthiah Annamalai <ezhillang@gmail.com>
2015-05-07 00:49:04 -0400

    1) heuristic rules 2) tests for #1 3) dictionary loader utilities

---
+++ solthiruthi/dictionary.py
@@ -0,0 +1,50 @@
+## -*- coding: utf-8 -*-
+## (C) 2015 Muthiah Annamalai,
+## 
+from __future__ import print_function
+import abc
+import sys
+import codecs
+import argparse
+from tamil import utf8
+from pprint import pprint
+
+PYTHON3 = (sys.version[0] == '3')
+
+# TODO: add methods for loading TamilVU, Wikipedia and Project Madurai
+#       cleaned up data
+
+# specify dictionary interface without specifying storage
+class Dictionary:
+    __metaclass__ = abc.ABCMeta
+    
+    @abc.abstractmethod
+    def add(self,word,defns):
+        return
+    
+    @abc.abstractmethod
+    def isWord(self,word):
+        return
+    
+    @abc.abstractmethod
+    def getAllWords(self):
+        return
+        
+    @abc.abstractmethod
+    def getAllWordsIterable(self):
+        for word in self.getAllWords():
+            yield word
+        raise StopIteration
+    
+    def loadWordFile(self,filename):
+        # words will be loaded from the file into the Trie structure
+        with codecs.open(filename,'r','utf-8') as fp:
+            # 2-3 compatible
+            for word in fp.readlines():
+                self.add(word.strip())
+        return
+
+class TamilVU(Dictionary):
+    def __init__(self):
+        self.store = dict()
+    
