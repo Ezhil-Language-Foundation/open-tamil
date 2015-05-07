216a56a5655aeac8f4407ef07a6d38e24422c945
Muthiah Annamalai <ezhillang@gmail.com>
2015-05-07 01:34:53 -0400

    1) added rules for RepeatedLetters, AdjacentConsonants 2) refactored code for all hierarchy/Rules derived classes 3) updated tests 4) MIT license for open-tamil
    Ref:  Solthiruthi - heuristic rules #45 (issue)

--- tests/solthiruthi_rules.py
+++ tests/solthiruthi_rules.py
@@ -2,7 +2,7 @@
 # (C) 2015 Muthiah Annamalai
 
 from opentamiltests import *
-from solthiruthi.heuristics import AdjacentVowels
+from solthiruthi.heuristics import AdjacentVowels, AdjacentConsonants, RepeatedLetters
 
 class AdjacentUyirgalTest(unittest.TestCase):
     def test_word_in_error(self):
@@ -20,6 +20,42 @@ class AdjacentUyirgalTest(unittest.TestCase):
         expected[2] = False
         expected[7] = False
         self.assertEqual( actual, expected )
+
+class AdjacentMeiAgaram(unittest.TestCase):
+    def test_word_in_error(self):
+        obj = AdjacentConsonants()
+        rval = obj.apply(u"அஇங்ங்ஞாடி")
+        self.assertEqual(len(rval),2)
+        self.assertFalse(rval[0])
+        self.assertTrue(rval[1])
+        
+    def test_word(self):
+        obj = AdjacentConsonants()
+        in_words = u"டைட்டானிக் படத்தில் அஇங்ஞாடி அஇங்ங்ஞாடி அங்ஞாடிஇ ஜேக் மற்றும் ஆஅக்காள் ரோஸ் போன்று தன் காதலை வெளிப்படுத்தும் இரு தவளைகள்".split()
+        actual = [obj.apply(word)[0] for word in in_words]
+        expected = [True for i in range(0,len(in_words))]
+        expected[1] = False
+        expected[3] = False
+        expected[-1] = False
+        self.assertEqual( expected, actual )
+
+class RepeatedLettersTest(unittest.TestCase):
+    def test_word_in_error(self):
+        obj = RepeatedLetters()
+        rval = obj.apply(u"அஇங்ங்ஞாடி")
+        self.assertEqual(len(rval),2)
+        self.assertFalse(rval[0])
+        self.assertTrue(rval[1])
+        
+    def test_word(self):
+        obj = RepeatedLetters()
+        in_words = u"டைட்டானிக்க் பபடத்தில் அஇங்ஞாடி அஇங்ங்ஞாடி அங்ஞாடிஇ ஜேக் மற்றும் ஆஅக்காள் ரோஸ் போன்று தன் காதலை வெளிப்படுத்தும் இரு தவளைகள்".split()
+        actual = [obj.apply(word)[0] for word in in_words]
+        expected = [True for i in range(0,len(in_words))]
+        expected[0] = False
+        expected[1] = False
+        expected[3] = False
+        self.assertEqual( expected, actual )
     
 if __name__ == "__main__":
     unittest.main()
