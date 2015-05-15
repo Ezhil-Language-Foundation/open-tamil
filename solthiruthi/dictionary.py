c8e00ddb66ea2daf87449180da1cea518fc049dc
Muthiah Annamalai <ezhillang@gmail.com>
2015-05-15 19:48:43 -0400

    agarathi - class interface tuning

--- solthiruthi/dictionary.py
+++ solthiruthi/dictionary.py
@@ -17,7 +17,7 @@ class Dictionary:
     __metaclass__ = abc.ABCMeta
     
     @abc.abstractmethod
-    def add(self,word,defns):
+    def add(self,word):
         return
     
     @abc.abstractmethod
@@ -27,22 +27,66 @@ class Dictionary:
     @abc.abstractmethod
     def getAllWords(self):
         return
-        
+
     @abc.abstractmethod
     def getAllWordsIterable(self):
         for word in self.getAllWords():
             yield word
         raise StopIteration
     
-    def loadWordFile(self,filename):
+    def loadWordFile(self,filename,pre_processor=None):
         # words will be loaded from the file into the Trie structure
         with codecs.open(filename,'r','utf-8') as fp:
             # 2-3 compatible
             for word in fp.readlines():
-                self.add(word.strip())
+                if pre_processor:
+                    self.add( pre_processor(word.strip()) )
+                else:
+                    self.add(word.strip())
         return
 
-class TamilVU(Dictionary):
+class Agarathi(Dictionary):
+    def __init__(self):
+        self.store = list()
+        self.Finalized = False
+
+    def add(self,word):
+        if self.Finalized:
+            raise Exception("dictionary is finalized. cannot add more")
+        self.store.append(word)
+        return
+
+    def isWord(self,word):
+        return word in self.store
+
+    def finalize(self):
+        self.store = set(self.store)
+
+    @abc.abstractmethod
+    def getAllWords(self):
+        return
+
+    @abc.abstractmethod
+    def getAllWordsIterable(self):
+        for word in self.getAllWords():
+            yield word
+        raise StopIteration
+
+    def loadWordFile(self,filename,pre_processor=None):
+        # words will be loaded from the file into the Trie structure
+        with codecs.open(filename,'r','utf-8') as fp:
+            # 2-3 compatible
+            for word in fp.readlines():
+                if pre_processor:
+                    self.add( pre_processor(word.strip()) )
+                else:
+                    self.add(word.strip())
+        return
+
+class TamilVU(Agarathi):
     def __init__(self):
         self.store = dict()
-    
+
+class Wikipedia(Agarathi):
+    def __init__(self):
+        self.store = dict()
