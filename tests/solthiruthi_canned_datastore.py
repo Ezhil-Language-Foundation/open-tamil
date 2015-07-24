36a4ee65445757e89d73abbcc33c675e6fa97eba
Muthiah Annamalai <ezhillang@gmail.com>
2015-07-23 22:40:57 -0400

    Solthiruthi - framework - canned dictionary #74

---
+++ tests/solthiruthi_canned_datastore.py
@@ -0,0 +1,35 @@
+# -*- coding: utf-8 -*-
+# (C) 2015 Muthiah Annamalai
+
+from opentamiltests import *
+from solthiruthi.dictionary import *
+import sys
+import copy
+
+# Test the canned dictionary with data structure
+class TestDictionary(unittest.TestCase):
+    def setUp(self):
+        self.TVU,self.TVU_size = DictionaryBuilder.create(TamilVU)
+        
+    def tearDown(self):
+        del self.TVU
+#        del self.Madurai
+        
+    def test_factory_TVU(self):
+        TVU_agarathi,size = self.TVU,self.TVU_size
+        self.assertTrue(isinstance(TVU_agarathi,TamilVU))
+        self.assertEqual(size,63896)
+        return
+
+#    def test_factory_madurai(self):
+#        self.assertTrue(isinstance(self.Madurai,Madurai))
+#        self.assertEqual(size,63896)
+#        return
+        
+    def test_isword(self):
+        words = u"தமிழ் நாட்டில் சங்ககாலத்திலேயே ஒட்டியாணம் போன்ற இடையணிகள் இருந்தமைக்கான சான்றுகளும் பெண்".split(" ")
+        self.assertEqual(len(list(filter(self.TVU.isWord,words))),3)
+        self.assertTrue(self.TVU.isWord(u"தமிழ்"))
+        
+if __name__ == "__main__":
+    unittest.main()
