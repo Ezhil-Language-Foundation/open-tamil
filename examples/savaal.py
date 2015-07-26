a4e56bd7c0e1381f2c5895393994e32912a55b88
Muthiah Annamalai <ezhillang@gmail.com>
2015-07-26 15:31:02 -0400

    savaal : blog post @ https://ezhillang.wordpress.com/2015/07/26/open-tamil-%E0%AE%9A%E0%AE%B5%E0%AE%BE%E0%AE%B5%E0%AE%BE%E0%AE%B2%E0%AF%8D%E0%AE%B5%E0%AE%BE%E0%AE%9A%E0%AE%B2%E0%AF%8D%E0%AE%9A%E0%AE%B5%E0%AE%BE%E0%AE%B2%E0%AF%8D/

---
+++ examples/savaal.py
@@ -0,0 +1,14 @@
+# -*- coding: utf-8 -*-
+# (C) 2015 Muthiah Annamalai
+# 
+# This file is part of 'open-tamil' package tests
+
+import tamil
+import solthiruthi
+from solthiruthi.dictionary import *
+
+TVU_dict = DictionaryBuilder.create(TamilVU)
+word = u'சவால்’'
+q=list(tamil.wordutils.combinagrams(word,TVU_dict))
+print(u'|'.join(q))
+
