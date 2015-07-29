2cb26f0e44d0990616d1f9dd158cb4dfe4f1a0a8
Muthiah Annamalai <ezhillang@gmail.com>
2015-07-28 23:19:53 -0400

    fixup Python 2.6, Python 3 related test issues. Update Travis.yml file;

--- tamil/txt2unicode/orddic.py
+++ tamil/txt2unicode/orddic.py
@@ -1,3 +1,7 @@
+# -*- coding: utf-8 -*-
+# (C) 2014 Arulalan.T <arulalant@gmail.com>                                 
+# This file is part of open-tamil package
+
 try:
     # python 2.7 defaults comes with collections module
     from collections import OrderedDict
@@ -6,7 +10,4 @@ except Exception as e:
         # In python 2.2 to 2.6, user need to install ordereddict via pip
         from ordereddict import OrderedDict
     except Exception as e:
-        print(e, "No module found to import OrderedDict. So using normal dict itself")
         OrderedDict = dict 
-    # end of try:
-# end of try:
