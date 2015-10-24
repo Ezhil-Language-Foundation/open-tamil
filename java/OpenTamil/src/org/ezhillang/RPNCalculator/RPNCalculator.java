461117e2ee9f9527d4451fa5169b532bc5c77a2b
Muthiah Annamalai <ezhillang@gmail.com>
2015-10-24 13:02:16 -0400

    RPN Calculator update: 1) Use precedence and associativity rules for - and / operators 2) Add unary operators which have precendece higher than all binary ops    but no associativity

--- java/OpenTamil/src/org/ezhillang/RPNCalculator/RPNCalculator.java
+++ java/OpenTamil/src/org/ezhillang/RPNCalculator/RPNCalculator.java
@@ -9,8 +9,6 @@ import java.util.Iterator;
 import java.util.List;
 import java.util.Stack;
 
-import org.ezhillang.RPNCalculator.Token.Kinds;
-
 //represent a token within the calculator
 
 class Operand extends Token {
@@ -37,6 +35,7 @@ class Operator extends Token {
 }
 
 public class RPNCalculator {
+	boolean DEBUG = !true;
 	List<Token> m_toks;
 	Stack<Operand> operand_stack;
 	Stack<Operator> operator_stack;
@@ -44,13 +43,35 @@ public class RPNCalculator {
 	public RPNCalculator(List<Token> toks) {
 		m_toks = toks;
 		operand_stack  = new Stack<Operand>();
-		operator_stack  = new Stack<Operator>();
+		operator_stack  = new Stack<Operator>();		
 	}
 	
-	protected Operand simple_eval() throws Exception {
+	void display_stacks() {
+		if ( !DEBUG )
+			return;
+		
+		System.out.println("#############");
+		System.out.println("OPRTOR ="+operator_stack.size());		
+		Iterator<Operator> tok = operator_stack.iterator();
+		while(tok.hasNext()){
+			System.out.println(tok.next());
+		}
+		System.out.println("OPRND = "+operand_stack.size());
+		Iterator<Operand> tok2 = operand_stack.iterator();
+		while(tok2.hasNext()){
+			System.out.println(tok2.next());
+		}		
+		System.out.println("#############");
+		
+		return;
+	}
+	
+	protected void simple_eval() throws Exception {
+		display_stacks();
+		
 		Operator op = operator_stack.pop();
 		Token.Kinds op_kind = op.getKind();
-		
+			
 		Operand op_B = null, op_result = null;
 		Operand op_A = operand_stack.pop();
 		double dbl_op_A = op_A.getNumericValue(),dbl_op_B = Double.NaN,result = Double.NaN;
@@ -62,7 +83,7 @@ public class RPNCalculator {
 		} else if (  op_kind == Token.Kinds.SUB_OP ) {
 			op_B = operand_stack.pop();				
 			dbl_op_B = op_B.getNumericValue();
-			result = dbl_op_B - dbl_op_A;
+			result =  dbl_op_B - dbl_op_A;
 		} else if ( op_kind == Token.Kinds.MUL_OP ) {
 			op_B = operand_stack.pop();				
 			dbl_op_B = op_B.getNumericValue();
@@ -70,17 +91,45 @@ public class RPNCalculator {
 		} else if ( op_kind == Token.Kinds.DIV_OP ) {
 			op_B = operand_stack.pop();				
 			dbl_op_B = op_B.getNumericValue();
-			result = dbl_op_A / dbl_op_B;
+			result = dbl_op_B/dbl_op_A ;
+		} else if ( op_kind == Token.Kinds.LOG10_OP ) {
+			result = Math.log10(dbl_op_A);
+		} else if ( op_kind == Token.Kinds.LOG_OP ) {
+			result = Math.log(dbl_op_A);
+		} else if ( op_kind == Token.Kinds.EXP_OP ) {
+			result = Math.exp(dbl_op_A);
+		} else if ( op_kind == Token.Kinds.SIN_OP ) {
+			result = Math.sin(dbl_op_A);
+		} else if ( op_kind == Token.Kinds.COS_OP ) {
+			result = Math.cos(dbl_op_A);
+			
 		} else if ( op_kind == Token.Kinds.PERC_OP ) {
 			op_B = operand_stack.pop();
 			dbl_op_B = op_B.getNumericValue();
 			result = (100.00*dbl_op_B)/dbl_op_A ;
+		} else if ( op_kind == Token.Kinds.RPAREN ){
+			//RPARENS trigger calculations
+			while( operator_stack.peek().getKind() != Token.Kinds.LPAREN ) {
+				this.simple_eval();			
+			}
+			assert operator_stack.pop().getKind() == Token.Kinds.LPAREN;
+		} else if ( op_kind == Token.Kinds.LPAREN ) {
+			//consume LPARENS
+			return;
+		} else {
+			throw new Exception("Unknown operand kind "+op_kind.toString());
 		}
+		
 		op_result = new Operand(Double.toString(result),result);
-		return op_result;
+		operand_stack.push(op_result);
+		
+		if ( DEBUG )
+			System.out.println("Result => "+operand_stack.peek());
+		
+		return ;
 	}
 	
-	double eval() throws Exception {						
+	double eval() throws Exception {					
 		
 		Iterator<Token> tok_itr = m_toks.iterator();
 		while( tok_itr.hasNext() ) {
@@ -89,15 +138,36 @@ public class RPNCalculator {
 				operand_stack.push(new Operand(tok));
 				continue;
 			}
+			
 			//no precedence yet. this tok is an operator
-			operator_stack.push(new Operator(tok));
+			// *, /,% have higher precedence over +,-
+			if ( !operator_stack.isEmpty() ) {
+				Operator top_operator = operator_stack.peek();
+				while ( operator_stack.size() > 0 						
+						&& Token.isPrecedent(top_operator,tok)) {
+					//System.out.println("Precedence --->");					
+					this.simple_eval();					
+				}					
+			} 
+			
+			operator_stack.push(new Operator(tok));			
+			
+			if ( !operator_stack.isEmpty() && tok.getKind() == Token.Kinds.RPAREN ) {
+				while(     operator_stack.size() > 0 
+						&& operator_stack.peek().getKind() != Token.Kinds.LPAREN ) {
+					this.simple_eval();
+				}
+				assert operator_stack.pop().getKind() == Token.Kinds.LPAREN;
+			}
 		}
 		
 		while( !operator_stack.isEmpty() ) {
-			Operand op_result = this.simple_eval();
-			operand_stack.push(op_result);
+			this.simple_eval();			
 		}
 		
+		//overflow/underflow of stack indicates error in expr
+		assert operand_stack.size() == 1;
+		assert operator_stack.size() == 0;
 		
 		Operand val = operand_stack.pop();
 		return val.getNumericValue();
@@ -107,20 +177,49 @@ public class RPNCalculator {
 	 * @throws Exception 
 	 */
 	public static void main(String[] args) throws Exception {		
+		 
+		//basic trig: C^(0) + S^(0) =  1
+		test_pair( new String [] {"sin","3.1415","*","sin","3.1415"},1,true);//verbose
+		test_pair( new String [] {"sin","3.1415","*","sin","3.1415","+","cos","3.1415","*","cos","3.1415"},1,true);//verbose
 		
-		// TODO Auto-generated method stub
-		//System.out.println("Hello World!");
-		//String [] chunks = {"55","+","95","-","10"};
-		//String [] chunks = {"55","%","100.0","-","10"};
-		String [] chunks = {"5","*","4","-","3"};
+		//test brackets
+		//test_pair(new String [] {"(","5","-","3",")","*","2"},4);
+		test_pair(new String [] {"log10","10"},1);
+		test_pair( new String [] {"log",Double.toString(Math.E)}, 1.0 );
+				
+		test_pair( new String [] {"55","+","95","-","10"}, 140.0 );
+		test_pair( new String [] {"55","%","100.0","-","10"},45.0);
+		test_pair( new String [] {"5","*","4","-","3"}, +17.0);
+		test_pair( new String [] {"-37","-","10","-","5"},-52.0);
+		test_pair( new String [] {"100","/","10","/","5"},2.0);
+		test_pair(new String [] {"100","/","10","-","1","+", "1","/","5"},9.2);
+
+		//sub is left associative
+		test_pair(new String [] {"5","-","6","*","7","-","10"}, -47); 
+		//String [] chunks = {"(","5","-","6",")","*","(","7","-","10",")"}; //3
+		
+		//% % and / are also left associative
+		test_pair(new String [] {"5","%","10","%","20"}, 250);
+	
+	}
+	
+	static void test_pair(String [] chunks,double expected_val) throws Exception {
+		test_pair(chunks,expected_val,false);
+	}
+	
+	static void test_pair(String [] chunks,double expected_val,boolean verbose) throws Exception {
 		List<Token> toks = new ArrayList<Token>();
 		for(String chunk:chunks) {
 			toks.add( Token.parseToken(chunk) );
-			System.out.println(toks.get(toks.size()-1).toString());
+			if ( verbose )
+				System.out.println(toks.get(toks.size()-1).toString());
 		}
 		RPNCalculator rpnc = new RPNCalculator(toks);
-		System.out.println(rpnc.eval());
+		double actual = rpnc.eval();
+		if ( verbose )
+			System.out.println("Testing => "+chunks[0]+chunks[1]+"|actual =>"+actual);
 		
+		assert actual == expected_val;
 	}
 
 }
--- java/OpenTamil/src/org/ezhillang/RPNCalculator/Token.java
+++ java/OpenTamil/src/org/ezhillang/RPNCalculator/Token.java
@@ -1,9 +1,85 @@
 package org.ezhillang.RPNCalculator;
 
 public class Token {	
-	public static enum Kinds { NUMBER, LPAREN, RPAREN, FUNCTION, ADD_OP, SUB_OP, DIV_OP, MUL_OP, PERC_OP };
+	public static enum Kinds { NUMBER, LPAREN, RPAREN, FUNCTION, ADD_OP, SUB_OP, 
+		DIV_OP, MUL_OP, PERC_OP, LOG_OP, LOG10_OP, EXP_OP,SIN_OP,COS_OP,TAN_OP,
+		ASIN_OP,ATAN_OP,ACOS_OP,POWER_OP,UNARY_MINUS_OP,INV_OP,};
 	protected Kinds m_kind;
 	protected String m_raw;
+	public static boolean DEBUG = false;
+	
+	public static boolean isBinaryOp(Kinds k) {
+		final Kinds [] unary_ops = new Kinds [] { Kinds.ADD_OP,Kinds.SUB_OP,Kinds.MUL_OP,Kinds.DIV_OP,
+				Kinds.POWER_OP}; 
+		for( int itr=0; itr <unary_ops.length; itr++) {
+			if ( unary_ops[itr] == k)
+				return true;
+		}
+		return false;
+	}
+	
+	//number is not an OP
+	public static boolean isUnaryOp(Kinds k) {
+		final Kinds [] unary_ops = new Kinds [] { Kinds.LOG_OP, Kinds.LOG10_OP, Kinds.EXP_OP,Kinds.SIN_OP,Kinds.COS_OP,Kinds.TAN_OP,
+				Kinds.ASIN_OP,Kinds.ATAN_OP,Kinds.ACOS_OP, Kinds.UNARY_MINUS_OP, Kinds.INV_OP }; 
+		for( int itr=0; itr <unary_ops.length; itr++) {
+			if ( unary_ops[itr] == k)
+				return true;
+		}
+		return false;
+	}
+	
+	public static boolean isPrecedent(Token t1, Token t2) {
+		
+		Kinds first = t1.getKind(), second = t2.getKind();
+		
+		if ( DEBUG )
+			System.out.println(t1+"?"+t2);
+		
+		assert first != Kinds.NUMBER && second != Kinds.NUMBER;
+		
+		//unary ops take precedencce
+		if ( first != Kinds.RPAREN && second != Kinds.RPAREN ) {
+			if ( isUnaryOp(first) )
+				return true;
+			if ( isUnaryOp(second))
+				return false;
+		}
+		
+		int rank_A = 0,rank_B=0;
+		if ( first == Kinds.RPAREN) {
+			rank_A = 11;
+	    } else if(first == Kinds.DIV_OP || first == Kinds.MUL_OP ) {
+			rank_A = 10;
+		} else if (first == Kinds.PERC_OP) {
+			rank_A = 9;
+		} else if ( first == Kinds.SUB_OP) {
+			rank_A = 8;
+		} else if ( first == Kinds.LPAREN ) {
+			rank_A = 7;
+		} else {
+			rank_A = 0;
+		}
+		
+		if ( second == Kinds.RPAREN ) {
+			rank_B = 11;
+	    } else if(second == Kinds.DIV_OP || second == Kinds.MUL_OP ) {
+			rank_B = 10;
+		} else if (second == Kinds.PERC_OP) {
+			rank_B = 9;
+		} else if ( second == Kinds.SUB_OP) {
+			rank_B = 8;
+		} else if ( second == Kinds.LPAREN ) {
+			rank_B = 7;
+		} else {
+			rank_B = 0;
+		}
+		
+		if ( first == Kinds.SUB_OP && second == Kinds.SUB_OP )
+			return true;
+		
+		return rank_A >= rank_B;
+	}
 	
 	public String getRawToken() {
 		return m_raw;
@@ -39,7 +115,7 @@ public class Token {
 	}
 	
 	public static Token parseToken(String chunk) throws Exception {
-		String lean_chunk = chunk.trim();
+		String lean_chunk = chunk.trim().toLowerCase();
 		if (lean_chunk == "+") {
 			return new Token(lean_chunk,Kinds.ADD_OP);
 		} else if ( lean_chunk == "-") {
@@ -54,7 +130,20 @@ public class Token {
 			return new Token(lean_chunk,Kinds.LPAREN);
 		} else if ( lean_chunk == ")" ) {
 			return new Token(lean_chunk,Kinds.RPAREN);			
+		} else if ( lean_chunk == "log10" ) {
+			return new Token(lean_chunk,Kinds.LOG10_OP);
+		} else if ( lean_chunk == "log" ) {
+			return new Token(lean_chunk,Kinds.LOG_OP);			
+		} else if ( lean_chunk == "exp" ) {
+			return new Token(lean_chunk,Kinds.EXP_OP);		
+		} else if ( lean_chunk == "sin" ) {
+			return new Token(lean_chunk,Kinds.SIN_OP);		
+		} else if ( lean_chunk  == "cos" ) {
+			return new Token(lean_chunk,Kinds.COS_OP);
 		}
+		
+		//LOG10_OP, EXP_OP,SIN_OP,COS_OP,TAN_OP,ASIN_OP,ATAN_OP,ACOS_OP
+		
 		
 		try { 
 			//most likely a number
