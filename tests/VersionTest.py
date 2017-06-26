8bc201f47182b005c05d413a615f1bfff9e6dfb3
Muthiah Annamalai <ezhillang@gmail.com>
2017-06-25 23:14:52 -0700

    update version to 0.67

--- tests/VersionTest.py
+++ tests/VersionTest.py
@@ -10,7 +10,7 @@ import tamil
 
 class VersionTester(unittest.TestCase):
     def test_version(self):        
-        self.assertEqual(tamil.VERSION,'0.51')
+        self.assertEqual(tamil.VERSION,'0.67')
 
 if __name__ == '__main__':    
     unittest.main()
