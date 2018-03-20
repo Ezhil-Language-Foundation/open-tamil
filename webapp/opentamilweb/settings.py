de184aa4231bb37196cb8ac5887d14532fb62391
Syed <abuvanth@gmail.com>
2018-03-20 14:19:18 +0530

    Tamil Language Switcher

--- webapp/opentamilweb/settings.py
+++ webapp/opentamilweb/settings.py
@@ -63,6 +63,7 @@ TEMPLATES = [
                 'django.template.context_processors.debug',
                 'django.template.context_processors.request',
                 'django.contrib.auth.context_processors.auth',
+                'django.template.context_processors.i18n',
                 'django.contrib.messages.context_processors.messages',
             ],
         },
