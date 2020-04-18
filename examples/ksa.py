4cc9c9ebae9d02ab2f154aa41c4f7325a7324a59
Muthiah Annamalai <ezhillang@gmail.com>
2020-04-18 12:48:54 -0700

    Kutty Story - text analysis. /Users/muthu/Desktop$python3 ksa.py [Let, me, sing, a, குட்டி, story] 1 [Pay, attention,, listen, to, me] 0 [என்னண்ணா, "englishu?] 1 ['Just', 'listen', 'bro'] 0 ['Let', 'me', 'sing', 'a', 'குட்டி', 'story'] 1 ['Pay', 'attention,', 'listen', 'to', 'me'] 0 ['If', 'you', 'want', 'take', 'it', 'or', 'else', 'வேணாம்', 'tension'] 1 ['Leave', 'it', 'baby'] 0 ['Life', 'is', 'very', 'short', 'நண்பா'] 1 ['Always', 'be', 'happy'] 0 ['பலவித', 'problems', 'will', 'come', 'and', 'go'] 1 ['கொஞ்சம்', 'chill', 'பண்ணு', 'மாப்பி'] 3 ['Together', 'man'] 0 ['Let', 'me', 'sing', 'a', 'குட்டி', 'story'] 1 ['Pay', 'attention,', 'listen', 'to', 'me'] 0 ['If', 'you', 'want', 'take', 'it', 'or', 'else', 'வேணாம்', 'tension'] 1 ['Leave', 'it', 'baby'] 0 ['Life', 'is', 'very', 'short', 'நண்பா'] 1 ['Always', 'be', 'happy'] 0 ['Design', designah"] 0 [Problems, will, come, and, go] 0 [கொஞ்சம், chill, பண்ணு, மாப்பி] 3 [No, tension, baby] 0 ["Speedah, 'போனா,', 'கவனம்', 'must'] 2 [Slowah", போனா,, "steadyயும், 'must'] 2 ['Oh', 'anger', 'always', 'misery', 'baby'] 0 [Friendsah", நின்னா,, powerful, மாப்பி] 2 [Haters, are, gonna, hate, but, ignore, calmly] 0 [Negativity, எல்லாம், தள்ளி, வை, baby] 3 [Focus, on, what, you, dream, and, "dont, 'worry', 'மாப்பி'] 1 ['Postivity', 'உன்ன', 'lift', 'பண்ணும்', 'baby'] 2 ['Life', 'is', 'very', 'short', 'நண்பா'] 1 ['Always', 'be', 'happy'] 0 ['Very', 'many', 'problems', 'will', 'come', 'and', 'go'] 0 ['கொஞ்சம்', 'ஜில்', 'பண்ணு', 'மாப்பி'] 4 ['Students'] 0 ['Let', 'me', 'sing', 'a', 'குட்டி', 'story'] 1 ['Pay', 'attention,', 'listen', 'to', 'me'] 0 ['If', 'you', 'want', 'take', 'it', 'or', 'else', 'வேணாம்', 'tension'] 1 ['Leave', 'it', 'baby'] 0 ['Life', 'is', 'very', 'short', 'நண்பா'] 1 ['Always', 'be', 'happy'] 0 ['Design', designah"] 0 [Problems, will, come, and, go] 0 [கொஞ்சம், ஜில், பண்ணு, மாப்பி] 4 [No, tension, baby!] 0 ["Hardworkம், 'வேணும்'] 2 [Smartworkம்", வேணும்] 2 [Self, motivation, அது, நீதானே] 2 [Education, வேணும்] 1 [Dedication, வேணும்] 1 [Self, valuation, அத, பண்ணி, பாரேன்] 3 ["Dont, 'be', 'the', 'person', 'spreading', 'haterd', 'மாப்பி'] 1 ['பின்னாடி', 'பேசுறது', 'ரொம்ப', 'crappy'] 3 ['Always', 'be', 'polite', 'and', 'just', dont", be, nasty] 0 [You, will, be, the, reason, to, make, someone, happy] 0 [Life, is, very, short, நண்பா] 1 [Always, be, happy] 0 [பலவித, problems, will, come, and, go] 1 [கொஞ்சம், chill, பண்ணு, மாப்பி] 3 [One, last, time] 0 [Let, me, sing, a, குட்டி, story] 1 [Pay, attention,, listen, to, me] 0 [If, you, want, take, it, or, else, வேணாம், tension] 1 [Leave, it, baby] 0 [Life, is, very, short, நண்பா] 1 [Always, be, happy] 0 [Design, "designah] 0 ['Problems', 'will', 'come', 'and', 'go'] 0 ['கொஞ்சம்', 'chill', 'பண்ணு', 'மாப்பி'] 3 ['That', 'was', 'my', 'குட்டி', 'story'] 1 ['How', 'was', 'my', 'குட்டி', 'story?'] 1 ['That', 'was', 'my', 'குட்டி', 'story'] 1 ['How', 'was', 'my', 'குட்டி', 'story?'] 1 ['Just', awesomeண்ணா"] 1 [No, tension, baby] 0 English = 79.71014492753623%, Tamil = 20.28985507246377%

---
+++ examples/ksa.py
@@ -0,0 +1,23 @@
+#!/bin/env python3
+from codecs import open
+from tamil import utf8
+import re
+
+with open('kuttistory.txt','r','utf-8') as fp:
+    data = fp.readlines()
+
+class Stats:
+    __fields__ = ('total_words','tamil_words')
+stats = Stats()
+stats.total_words=0.0
+stats.tamil_words=0.0
+
+for line in data:
+    all_words = re.split('\s+',line.strip())
+    ta_words = list(utf8.get_tamil_words(utf8.get_letters(line)))
+    print(all_words,len(ta_words))
+    stats.tamil_words += len( ta_words )
+    stats.total_words += len(all_words)
+#tamil fraction
+taf = float(stats.tamil_words)/stats.total_words
+print('English = {0}%, Tamil = {1}%'.format(100.0*(1-taf),100.0*(taf)))
