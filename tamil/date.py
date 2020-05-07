c70aa1212be26dba590ca6bd68fb6c1090d87fd2
Muthiah Annamalai <ezhillang@gmail.com>
2020-05-06 23:50:01 -0700

    Update bugs in module tamil.date       - make this Python-3 compatible Add tests for exploring Thirukkural Corpus based on writer Nanjilnadan: நாஞ்சில் அவர்கள் எழுதியதாவது:         https://nanjilnadan.com/2020/04/09/%e0%ae%95%e0%af%8a%e0%ae%9f%e0%af%81%e0%ae%aa%e0%af%8d%e0%ae%aa%e0%ae%be%e0%ae%b0%e0%af%8d%e0%ae%87%e0%ae%b2%e0%ae%be%e0%ae%a9%e0%af%81%e0%ae%ae%e0%af%8d/
            ̀எடுத்துக்காட்டுக்கு, புழுதி என்ற சொல் 1037வது குறளில் கையாளப்பட்டுள்ளது.
            பழம் என்ற சொல் 1120 வது குறளில் உண்டு.
            முலை என்ற சொல் 402 மற்றும் 1087 வது குறள்களில்.
            அனிச்சம் எனும் சொல் 90வது, 1111வது, 1115 வது, 1120 வது திருக் குறள்களில் உண்டு.̀

--- tamil/date.py
+++ tamil/date.py
@@ -2,7 +2,7 @@
 # (C) 2015 Muthiah Annamalai
 # This file is part of open-tamil project
 
-from __future__ import print_function
+
 import time
 import sys
 PYTHON3 = sys.version > '3'
@@ -14,10 +14,9 @@ if PYTHON3:
 class BasicTamilTimeFormat:
     @staticmethod
     def format(year,month,month_day,week_day,hour,minute,second):
-        print(DateUtils.tamil_month(month),month)
-        print(DateUtils.tamil_weekday(week_day), week_day)
-        print(DateUtils.get_hour_prefix(hour),hour)
-    
+        return " ".join([DateUtils.tamil_month(month), str(week_day), DateUtils.tamil_weekday(week_day),
+        DateUtils.get_hour_prefix(hour),str(minute),str(second)])
+
 #
 # Index Attribute       Values
 # 0     tm_year (for example, 1993)
@@ -30,51 +29,61 @@ class BasicTamilTimeFormat:
 # 7     tm_yday range [1, 366]
 # 8     tm_isdst        0, 1 or -1; see below
 
+TamilLunarMonths = list(zip(["சித்திரை","வைகாசி","ஆனி","ஆடி","ஆவணி","புரட்டாசி",
+"ஐப்பசி","கார்த்திகை",
+"மார்கழி","தை","மாசி","பங்குனி"],[30,31,31,31,31,30,29,29,29,29,29,30]))
+
+TamilSeasonsMonths = {"இளவேனில்":["சித்திரை","வைகாசி"],
+"கோடை":["ஆனி","ஆடி"],
+"கார்":["ஆவணி","புரட்டாசி"],
+"குளிர்":["ஐப்பசி","கார்த்திகை"],
+"பனி":["மார்கழி","தை","மாசி","பங்குனி"]
+}
 
 # 0-24
 class DateUtils:
-    YEAR = u"ஆண்டு"
-    WEEK = u"வாரம்"
-    MONTH = u"மாதம்"
-    DAY = u"நாள்"
-    DAY_SUFFIX = u"கிழமை"
-    MINUTE = u"நிமிடம்"
-    HOUR = u"மணி"
-    TIME = u"நேரம்"
-    WEEKDAYS_INDEX = [u"monday",u"tuesday",u"wednesday",u"thursday",u"friday",u"saturday",u"sunday"]
-    WEEKDAYS = {u"monday" : u"திங்கள்",
-                u"tuesday" : u"செவ்வாய்",
-                u"wednesday" : u'புதன்',
-                u"thursday" : u'வியாழன்',
-                u'friday':u'வெள்ளி',
-                u'saturday':u'சனிக்கிழமை',
-                u'sunday':u'ஞாயிறு',
+    YEAR = "ஆண்டு"
+    WEEK = "வாரம்"
+    MONTH = "மாதம்"
+    DAY = "நாள்"
+    DAY_SUFFIX = "கிழமை"
+    MINUTE = "நிமிடம்"
+    HOUR = "மணி"
+    TIME = "நேரம்"
+    WEEKDAYS_INDEX = ["monday","tuesday","wednesday","thursday","friday","saturday","sunday"]
+    WEEKDAYS = {"monday" : "திங்கள்",
+                "tuesday" : "செவ்வாய்",
+                "wednesday" : 'புதன்',
+                "thursday" : 'வியாழன்',
+                'friday':'வெள்ளி',
+                'saturday':'சனிக்கிழமை',
+                'sunday':'ஞாயிறு',
                 }
     MONTHS_INDEX = [None,
-                    u"January",
-                    u"February",
-                    u"March",
-                    u"April",
-                    u"May",
-                    u"June",
-                    u"July",
-                    u"August",
-                    u"September",
-                    u"October",
-                    u"November",
-                    u"December"]
-    MONTHS = {u'January':u'ஜனவரி',
-              u'February':u'பிப்ரவரி',
-              u'March':u'மார்ச்',
-              u'April':u'ஏப்ரல்',
-              u'May':u'மே',
-              u'June':u'ஜூன்',
-              u'July':u'ஜூலை',
-              u'August':u'ஆகஸ்ட்',
-              u'September':u'செப்டம்பர்',
-              u'October':u'அக்டோபர்',
-              u'November':u'நவம்பர்',
-              u'December':u'டிசம்பர்'}
+                    "January",
+                    "February",
+                    "March",
+                    "April",
+                    "May",
+                    "June",
+                    "July",
+                    "August",
+                    "September",
+                    "October",
+                    "November",
+                    "December"]
+    MONTHS = {'January':'ஜனவரி',
+              'February':'பிப்ரவரி',
+              'March':'மார்ச்',
+              'April':'ஏப்ரல்',
+              'May':'மே',
+              'June':'ஜூன்',
+              'July':'ஜூலை',
+              'August':'ஆகஸ்ட்',
+              'September':'செப்டம்பர்',
+              'October':'அக்டோபர்',
+              'November':'நவம்பர்',
+              'December':'டிசம்பர்'}
 
     @staticmethod
     def tamil_weekday(week_day):
@@ -85,47 +94,45 @@ class DateUtils:
     def tamil_month(month):
         key = DateUtils.MONTHS_INDEX[month]
         return DateUtils.MONTHS[key]
-    
+
     @staticmethod
     def get_time(local_time=None,fmt=None):
         if not local_time:
             local_time = time.localtime()
-        if not fmt:            
+        if not fmt:
             fmt = BasicTamilTimeFormat
         year = local_time.tm_year
         month_day = local_time.tm_mday
         month = local_time.tm_mon
         week_day =local_time.tm_wday
-        year_day = local_time.tm_yday            
-        hour,minute,second = local_time.tm_hour,local_time.tm_min,local_time.tm_
-        
-        fmt.format(year,month,month_day,week_day,hour,minute,second)
-        return None
-    
+        year_day = local_time.tm_yday
+        hour,minute,second = local_time.tm_hour,local_time.tm_min,local_time.tm_sec
+        return fmt.format(year,month,month_day,week_day,hour,minute,second)
+
     @staticmethod
     def get_hour_prefix(hour):
         assert ( hour >= 0 and hour <= 24)# "hour variable should be in [0,24] c
         if (hour <= 3) or (hour  >= 12+11):
-            prefix = u"நள்ளிரவு" #u"nalliravu"
+            prefix = "நள்ளிரவு" #u"nalliravu"
         elif hour <= 6:
-            prefix = u"அதிகாலை" #u"vidikalai"
+            prefix = "அதிகாலை" #u"vidikalai"
         elif hour < 12:
-            prefix = u"காலை"#u"kalai"
+            prefix = "காலை"#u"kalai"
         elif hour < (12+3):
-            prefix = u"மத்தியானம்" #u"mathiyam"
+            prefix = "மத்தியானம்" #u"mathiyam"
         elif hour < (12+7):
-            prefix = u"மாலை" #u"malai"
+            prefix = "மாலை" #u"malai"
         elif hour < (12+11):
-            prefix = u"இரவு" #u"iravu"
+            prefix = "இரவு" #u"iravu"
         else:
             assert False
-        return prefix    
+        return prefix
 
-if __name__ ==  u"__main__":
+if __name__ ==  "__main__":
     for hr in range(0,25):
         print(hr,DateUtils.get_hour_prefix(hr))
-    for wkday_EN, wkday_TA in DateUtils.WEEKDAYS.items():
+    for wkday_EN, wkday_TA in list(DateUtils.WEEKDAYS.items()):
         print(wkday_EN, '->', wkday_TA)
-    for wkday_EN, wkday_TA in DateUtils.MONTHS.items():
+    for wkday_EN, wkday_TA in list(DateUtils.MONTHS.items()):
         print(wkday_EN, '->', wkday_TA)
-    DateUtils.get_time(time.localtime())
+    print(DateUtils.get_time(time.localtime()))
--- tests/ImportTester.py
+++ tests/ImportTester.py
@@ -5,23 +5,53 @@
 #
 
 # setup the paths
+import time
 from opentamiltests import *
+
+import tamil
+from tamil.date import DateUtils
+from kural import Kural, Thirukkural
 
 class ImportTester(unittest.TestCase):
     def test_import_tester(self):
         import tamil; import ngram; import transliterate
+        self.assertTrue(True)
 
     def test_import_num2tamilstr(self):
         from tamil.numeral import num2tamilstr, num2tamilstr_american
         for key in ['num2tamilstr','num2tamilstr_american']:
-            assert locals()[key]
+            self.assertTrue( locals()[key] )
+
+class MiscTestMay2020(unittest.TestCase):
+    def test_date_counts(self):
+        from tamil.date import TamilLunarMonths, TamilSeasonsMonths
+        self.assertEqual( len(TamilLunarMonths) , 12)
+        self.assertEqual( sum([d for _,d in TamilLunarMonths]), 359)
+        self.assertEqual( sum([len(v) for _,v in TamilSeasonsMonths.items()]), 12)
+
+    def test_date_fmt(self):
+        self.assertTrue( DateUtils.get_time(time.localtime()) )
 
     def test_kural(self):
-        from kural import Kural, Thirukkural
         kk = Thirukkural()
         self.assertTrue( kk )
         self.assertTrue( u"ஆதி"  in kk.db[0].ta)
         self.assertTrue(u"பகவன்" in kk.db[0].ta)
+
+    def test_kural_iter(self):
+        """
+        நாஞ்சில் அவர்கள் எழுதியதாவது:
+        https://nanjilnadan.com/2020/04/09/%e0%ae%95%e0%af%8a%e0%ae%9f%e0%af%81%e0%ae%aa%e0%af%8d%e0%ae%aa%e0%ae%be%e0%ae%b0%e0%af%8d%e0%ae%87%e0%ae%b2%e0%ae%be%e0%ae%a9%e0%af%81%e0%ae%ae%e0%af%8d/
+
+        ̀எடுத்துக்காட்டுக்கு, புழுதி என்ற சொல் 1037வது குறளில் கையாளப்பட்டுள்ளது.
+        பழம் என்ற சொல் 1120 வது குறளில் உண்டு.
+        முலை என்ற சொல் 402 மற்றும் 1087 வது குறள்களில்.
+        அனிச்சம் எனும் சொல் 90வது, 1111வது, 1115 வது, 1120 வது திருக் குறள்களில் உண்டு.̀
+        """
+        நாஞ்சில் = { 'அனிச்ச': [90,1111,1115,1120],'புழுதி':[1037],'பழம்':[1120], 'முலை':[402,1087]}
+        for சொல்,இடம்  in நாஞ்சில்.items():
+            கண்ட_இடம் = Thirukkural.occurrence(சொல்)
+            self.assertEqual( கண்ட_இடம், இடம்)
 
 if __name__ == '__main__':
     unittest.main()
