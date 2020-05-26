8ac0de583f65b0062b2a53e44f12e1d9a0368cc0
Muthiah Annamalai <ezhillang@gmail.com>
2020-05-26 12:15:21 -0700

    ITRANS transliteration scheme module

--- transliterate/__init__.py
+++ transliterate/__init__.py
@@ -3,6 +3,7 @@ from . import jaffna
 from . import azhagi
 from . import combinational
 from . import UOM
+from . import itrans
 from .algorithm import Iterative, Greedy, Tamil2English, reverse_transliteration_table
 
 #Transliteration module
