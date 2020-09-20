ce6ceee97b5bc8d71413fe82fa292ee22d6db357
Muthiah Annamalai <ezhillang@gmail.com>
2020-09-20 12:35:52 -0700

    Kural Gaussian fit: yields mean=29.5 mathirai and std=+/-2.5 after Gaussian curve fit

--- examples/kural_mathirai.py
+++ examples/kural_mathirai.py
@@ -6,6 +6,13 @@ from collections import Counter, OrderedDict
 from pprint import pprint
 import matplotlib.pyplot as plt
 import matplotlib
+import numpy as np
+from scipy.optimize import curve_fit
+
+# Define model function to be used to fit to the data above:
+def gauss(x, *p):
+    A, mu, sigma = p
+    return A*np.exp(-(x-mu)**2/(2.*sigma**2))
 
 def main():
     eq = Counter()
@@ -25,10 +32,24 @@ def main():
     pprint(eqd)
     print("total = ",sum(eq.values()))
     plt.scatter(eq_sorted.keys(),eq_sorted.values())
-    plt.title(u'குறள் மாத்திரை வரிசை',{'fontname':'Catamaran'})
     plt.ylabel(u'குறட்பாக்கள் எண்ணிக்கை',{'fontname':'Catamaran'})
     plt.xlabel(u'மாத்திரை அளவு',{'fontname':'Catamaran'}) #Arial Unicode MS'})
+
+    # p0 is the initial guess for the fitting coefficients (A, mu and sigma above)
+    p0 = [75., 20., 5.]
+    coeff, var_matrix = curve_fit(gauss, list(eq_sorted.keys()), list(eq_sorted.values()), p0=p0)
+
+    # Get the fitted curve
+    hist_fit = gauss(list(eq_sorted.keys()), *coeff)
+    plt.plot(eq_sorted.keys(), hist_fit, label='Gaussian Fitted data (mean=%g, std=%g)'%(coeff[1],coeff[2]))
+    plt.title(r'குறள் மாத்திரை வரிசை (Gauss \mu=%g, \sigma=%g)'%(coeff[1],coeff[2]),{'fontname':'Catamaran'})
+
+    # Finally, lets get the fitting parameters, i.e. the mean and standard deviation:
+    print ('Fitted mean = ', coeff[1])
+    print('Fitted standard deviation = ', coeff[2])
+
     plt.show()
+
 
 if __name__ == "__main__":
     main()
