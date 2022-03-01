7aa2ab6854ca87d9eceabf8d6c8cc0adbaad2f89
aathi2002 <68262020+aathi2002@users.noreply.github.com>
2022-03-01 05:35:25 -0600

    update
    add a new line in the python file

--- keyboard/bamini.py
+++ keyboard/bamini.py
@@ -8,3 +8,9 @@ class Bamini(Layout):
     def load_layout(self):
         self.keymap[u"`"] = u"ஹ|`"
         self.keymap[u"1"] = u"1|!"
+        
+class Bamini(Layout):
+    def load_layout(self):
+        self.keymap[u"`"] = u"ஹ|`"
+        self.keymap[u"1"] = u"1|!"
+
